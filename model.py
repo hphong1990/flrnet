5a1c1d0c0570f9395ba74377776680e5c13c88fe
hphong1990 <phongnch90@gmail.com>
2024-08-19 10:25:59 +0700

    baseline_impl

--- model.py
+++ model.py
@@ -77,11 +77,19 @@ def vgg_decoder(input_shape = (4,8,4), n_base_features = 64):
 def create_mapping_operator(no_of_sensor = 8, latent_dim = (4,8,4)):
     inputs = keras.Input(shape = (no_of_sensor))
     fc_1 = Dense(128, activation=LeakyReLU(0.2))(inputs)
-    fc_2 = Dense(256, activation=LeakyReLU(0.2))(fc_1)
-    fc_3 = Dense(512, activation=LeakyReLU(0.2))(fc_2)
-    fc_3 = Dense(256, activation=LeakyReLU(0.2))(fc_2)
-    fc_4 = Dense(128)(fc_3)
-    latent_var = Reshape(target_shape=latent_dim)(fc_4)
+    bn_1 = BatchNormalization()(fc_1)
+    fc_2 = Dense(256, activation=LeakyReLU(0.2))(bn_1)
+    bn_2 = BatchNormalization()(fc_2)
+
+    fc_3 = Dense(512, activation=LeakyReLU(0.2))(bn_2)
+    bn_3 = BatchNormalization()(fc_3)
+
+
+    fc_4 = Dense(256, activation=LeakyReLU(0.2))(bn_3)
+    bn_4 = BatchNormalization()(fc_4)
+
+    fc_5 = Dense(128)(bn_4)
+    latent_var = Reshape(target_shape=latent_dim)(fc_5)
     z_mean = layers.Conv2D(latent_dim[2],3, padding="same",name="z_mean")(latent_var)
     z_log_var = layers.Conv2D(latent_dim[2],3, padding="same",name="z_log_var")(latent_var)
     z = flr_layer.Sampling()([z_mean,z_log_var])
@@ -105,15 +113,14 @@ class FLRNet(keras.Model):
     def __init__(self,  n_sensor = 8, **kwargs):
         super().__init__(**kwargs)
         self.encoder = vgg_encoder()
+        self.encoder.trainable = False
         self.decoder = vgg_decoder()
+        self.decoder.trainable = False
         self.sens_mapping = create_mapping_operator(no_of_sensor=n_sensor)
-        # self.vgg19 = vgg()
+        self.vgg19 = vgg()
+    
 
         self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
-        self.reconstruction_loss_tracker = keras.metrics.Mean(
-            name="reconstruction_loss_ae"
-        )
-        self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss_ae")
         self.sens_reconstruction_loss_tracker = keras.metrics.Mean(
             name="reconstruction_loss_sens"
         )
@@ -123,8 +130,6 @@ class FLRNet(keras.Model):
     def metrics(self):
         return [
             self.total_loss_tracker,
-            self.reconstruction_loss_tracker,
-            self.kl_loss_tracker,
             self.sens_reconstruction_loss_tracker,
             self.sens_kl_loss_tracker,
         ]
@@ -150,6 +155,77 @@ class FLRNet(keras.Model):
         img_inp = tf.cast(data[1],dtype = tf.float32)
         with tf.GradientTape() as tape:
             # Autoencoder
+            z_mean_ae, z_log_var_ae, _ = self.encoder(img_inp)
+            
+            # Sens recon
+            z_mean_sens, z_log_var_sens, z_sens = self.sens_mapping(sens_inp)
+            reconstruction_sens = self.decoder(z_sens)
+            reconstruction_loss_sens = tf.keras.losses.MeanAbsoluteError(reduction = 'sum')(reconstruction_sens,img_inp)
+            
+            kl_loss_sens = self.kld(z_mean_sens,z_mean_ae,z_log_var_sens, z_log_var_ae)
+            kl_loss_sens = (tf.reduce_sum(kl_loss_sens, axis=(1,2,3)))
+
+            perceptual_loss_sens = self.perceptual_loss(reconstruction_sens,img_inp)
+
+            total_loss = reconstruction_loss_sens + kl_loss_sens + perceptual_loss_sens
+            
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        self.total_loss_tracker.update_state(total_loss)
+        self.sens_reconstruction_loss_tracker.update_state(reconstruction_loss_sens)
+        self.sens_kl_loss_tracker.update_state(kl_loss_sens)
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss_sens": self.sens_reconstruction_loss_tracker.result(),
+            "kl_loss_sens": self.sens_kl_loss_tracker.result(),
+        }
+    
+
+# Trainer class
+class VAE(keras.Model):
+    def __init__(self,  n_sensor = 8, **kwargs):
+        super().__init__(**kwargs)
+        self.encoder = vgg_encoder()
+        self.decoder = vgg_decoder()
+        self.vgg19 = vgg()
+    
+        self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
+        self.reconstruction_loss_tracker = keras.metrics.Mean(
+            name="reconstruction_loss_ae"
+        )
+        self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss_ae")
+
+        
+    @property
+    def metrics(self):
+        return [
+            self.total_loss_tracker,
+            self.reconstruction_loss_tracker,
+            self.kl_loss_tracker,
+        ]
+    def kld(self, mean_1, mean_2, z_log_var_1, z_log_var_2):
+        var_1 = tf.exp(z_log_var_1)
+        var_2 = tf.exp(z_log_var_2)
+        kl_loss = ( 
+            tf.math.log((var_2 / var_1) ** 0.5) 
+              + (var_1 + (mean_1 - mean_2) ** 2) / (2 * var_2) 
+              - 0.5
+           )
+        return kl_loss
+    
+    def perceptual_loss(self, y_pred, gt):
+        # Pred perceptaul
+        pred_feature = self.vgg19(y_pred)
+        # GT perceptual
+        gt_feature = self.vgg19(gt)
+        return tf.keras.losses.MeanSquaredError(reduction = 'sum')(pred_feature,gt_feature)
+    
+    def train_step(self, data):
+        # sens_inp = tf.cast(data[0], dtype = tf.float32)
+
+        img_inp = tf.cast(data[1],dtype = tf.float32)
+        with tf.GradientTape() as tape:
+            # Autoencoder
             z_mean_ae, z_log_var_ae, z_ae = self.encoder(img_inp)
             reconstruction_ae = self.decoder(z_ae)
             reconstruction_loss_ae = tf.keras.losses.MeanAbsoluteError(reduction = 'sum')(reconstruction_ae,img_inp)
@@ -168,32 +244,17 @@ class FLRNet(keras.Model):
             # z_dot_compute = acc.jvp(z_mean_metric)
             # metric_loss = tf.keras.losses.MeanAbsoluteError(reduction = 'sum')(z_dot_compute, z_dot_enc)
 
-            # perceptual_loss_ae = self.perceptual_loss(reconstruction_ae,img_inp)
+            perceptual_loss_ae = self.perceptual_loss(reconstruction_ae,img_inp)
 
-            # Sens recon
-            z_mean_sens, z_log_var_sens, z_sens = self.sens_mapping(sens_inp)
-            reconstruction_sens = self.decoder(z_sens)
-            reconstruction_loss_sens = tf.keras.losses.MeanAbsoluteError(reduction = 'sum')(reconstruction_sens,img_inp)
-            
-            kl_loss_sens = self.kld(z_mean_sens,z_mean_ae,z_log_var_sens, z_log_var_ae)
-            kl_loss_sens = (tf.reduce_sum(kl_loss_sens, axis=(1,2,3)))
-
-            # perceptual_loss_sens = self.perceptual_loss(reconstruction_sens,img_inp)
-
-            total_loss = reconstruction_loss_ae + kl_loss_ae + 2*reconstruction_loss_sens + 2*kl_loss_sens 
-            # + perceptual_loss_ae + perceptual_loss_sens
+            total_loss = reconstruction_loss_ae + kl_loss_ae + perceptual_loss_ae
             
         grads = tape.gradient(total_loss, self.trainable_weights)
         self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
         self.total_loss_tracker.update_state(total_loss)
         self.reconstruction_loss_tracker.update_state(reconstruction_loss_ae)
         self.kl_loss_tracker.update_state(kl_loss_ae)
-        self.sens_reconstruction_loss_tracker.update_state(reconstruction_loss_sens)
-        self.sens_kl_loss_tracker.update_state(kl_loss_sens)
         return {
             "loss": self.total_loss_tracker.result(),
             "reconstruction_loss_ae": self.reconstruction_loss_tracker.result(),
             "kl_loss_ae": self.kl_loss_tracker.result(),
-            "reconstruction_loss_sens": self.sens_reconstruction_loss_tracker.result(),
-            "kl_loss_sens": self.sens_kl_loss_tracker.result(),
         }
